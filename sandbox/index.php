8aadb7e5f7b59110a6b6601a65a7588d6487408b
nodilod <nodilod@gmail.com>
2022-01-25 16:22:32 +0100

    mise en place environnement

--- sandbox/index.php
+++ sandbox/index.php
@@ -1,5 +1,50 @@
-<pre>
 <?php
-echo nl2br(file_get_contents("../README.md"));
+    include "header.php";
 ?>
-</pre>
+
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <p>Back to <a href="../examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <p class="lead">Pin a footer to thghfhcbklxjflkbcjndklfjcbn kdjgcvnb fgjcvbn fxkjcbnv dfkjcvxbnv idfgkjvcbnv fkjvcbn fgvkjcbn  fkvjcbn fgivjkcn fgijvkcxbn figkjvcbn ifgkvcbn ifgkjvcbn vigfjkvcbn ifgjvck,bn fijvkc nfigvjkcbn fgkjvcbe bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1><h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1><h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1><h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
+    
+</div>
+
+</main>
+<?php
+    include "tail.php";
+?>
