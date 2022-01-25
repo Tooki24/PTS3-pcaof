fc349d071b2e78400ccaf0f95b39a9d0325f8acf
Ambroize <enora.mahe17000@gmail.com>
2022-01-25 17:42:56 +0100

    footer
    modif footer et custom.css

--- sandbox/footer.php
+++ sandbox/footer.php
@@ -1,5 +1,11 @@
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer mt-auto py-3 custom-color ">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted row">
+        <div class="col lien_footer">
+            <a class="col lien_footer_in" href="">Mentions légales</a>
+            <a class="col lien_footer_in" href="">Plan du site</a>
+        </div>
+       <a class="btn col btn-primary custom-color-bouton" href="">Contact</a>
+    </span>
   </div>
 </footer>
