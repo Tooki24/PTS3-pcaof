52555724f8bcaae0f79ad1d6b996dd824e266303
Louis Bouclaud <louis.bouclaud@etudiant.univ-lr.fr>
2022-01-25 14:08:52 +0100

    Ajouts bootstrap / Revue Controller
    - ajout des fichiers pour bootstrap et ajout des lignes pour l'utiliser
    
    - commencement du controller revue

--- projet/src/Controller/RevueController.php
+++ projet/src/Controller/RevueController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Revue;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RevueRepository;
 
 class RevueController extends AbstractController
 {
-    #[Route('/revue', name: 'revue')]
-    public function index(): Response
+    #[Route('/revues', name: 'revues')]
+    public function index(RevueRepository $revueRepository): Response
     {
         return $this->render('revue/index.html.twig', [
             'controller_name' => 'RevueController',
+            'revues' => $revueRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/revues/liste_articles/{idRevue}', name: 'liste_articles')]
+    public function list_articles(RevueRepository $revueRepository, int $idRevue): Response
+    {
+        $revue = $revueRepository->find($idRevue);
+        return $this->render('revue/index.html.twig', [
+            'controller_name' => 'RevueController',
+            'revue' => $revue,
+            'articles'=>$revue->getArticles(),
         ]);
     }
 }
