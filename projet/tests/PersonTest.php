223b11323a7d5fc006749fd241bb3a79560bc37b
Mathis Fumel <fml.mathis@gmail.com>
2022-02-17 12:17:06 +0100

    Test Entity
    Nouveau Test Entity

--- projet/tests/PersonTest.php
+++ projet/tests/PersonTest.php
@@ -12,10 +12,22 @@ class PersonTest extends TestCase
         $person = new Person();
 
         $person->setName('name')
-        ->setFirstName('firstName');
+        ->setFirstName('firstName')
+        ->setSlug('slug')
+        ->setRole('role')
+        ->setIsOffice(1)
+        ->setPhotoName(null)
+        ->setPhotoFile(null);
 
         $this->assertTrue($person->getFirstName() === 'firstName');
         $this->assertTrue($person->getName() === 'name');
+        $this->assertTrue($person->getSlug() === 'slug');
+        $this->assertTrue($person->getRole() === 'role');
+        $this->assertTrue($person->getIsOffice() == 1);
+        $this->assertTrue($person->getPhotoName() === null);
+        $this->assertTrue($person->getPhotoFile() === null);
+
+
 
     }
     public function testIsFalse()
@@ -23,10 +35,19 @@ class PersonTest extends TestCase
         $person = new Person();
 
         $person->setName('name')
-            ->setFirstName('firstName');
+            ->setFirstName('firstName')
+            ->setSlug('slug')
+            ->setRole('role')
+            ->setIsOffice(1)
+            ->setPhotoName('imgName')
+            ->setPhotoFile(null);;
 
         $this->assertFalse($person->getFirstName() === 'firstNames');
         $this->assertFalse($person->getName() === 'names');
+        $this->assertFalse($person->getSlug() === 'slugS');
+        $this->assertFalse($person->getRole() === 'roles');
+        $this->assertFalse($person->getIsOffice() == 0);
+        $this->assertFalse($person->getPhotoName() === 'img');
 
     }
     public function testEmpty()
@@ -36,9 +57,15 @@ class PersonTest extends TestCase
         $this->assertEmpty($person->getId());
         $this->assertEmpty($person->getName());
         $this->assertEmpty($person->getFirstName());
-        $this->assertEmpty($person->getColloques());
         $this->assertEmpty($person->getArticles());
-        $this->assertEmpty($person->getInterventions());
         $this->assertEmpty($person->getPublications());
+        $this->assertEmpty($person->getSlug());
+        $this->assertEmpty($person->getRole());
+        $this->assertEmpty($person->getIsOffice());
+        $this->assertEmpty($person->getPhotoFile());
+        $this->assertEmpty($person->getPhotoName());
+
+
+
     }
 }
