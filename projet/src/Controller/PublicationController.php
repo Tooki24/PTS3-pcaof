3fe2d7a3d0ba0e30d97a677142c773100eb71eb3
Louis Bouclaud <louis.bouclaud@etudiant.univ-lr.fr>
2022-02-23 19:14:17 +0100

    Start & End Publications

--- projet/src/Controller/PublicationController.php
+++ projet/src/Controller/PublicationController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PublicationRepository;
 
 class PublicationController extends AbstractController
 {
     #[Route('/publications', name: 'publications')]
-    public function index(): Response
+    public function index(PublicationRepository $publicationRepository): Response
     {
         return $this->render('publication/index.html.twig', [
             'controller_name' => 'PublicationController',
+            'publications' => $publicationRepository->dateDesc()
+        ]);
+    }
+
+    #[Route('/publication/detail/{slug}', name: 'details_publication')]
+    public function details_publi(PublicationRepository $publicationRepository, string $slug): Response
+    {
+        return $this->render('publication/details_publication.html.twig', [
+            'controller_name' => 'PublicationController',
+            'publication' => $publicationRepository->findOneBy(['slug' => $slug]),
         ]);
     }
 }
